f1c9c9beec7f846b0bb328f8f031e1a9cf1cc744
bruholanda <brunaholandacunha7@gmail.com>
2022-09-19 12:41:44 -0300

    Integration tests;

---
+++ testesIntegracao/cacao-trybe/tests/integration/chocolates.test.js
@@ -0,0 +1,120 @@
+// tests/integration/chocolates.test.js
+
+const chai = require('chai');
+const sinon = require('sinon');
+const fs = require('fs');
+const chaiHttp = require('chai-http');
+const app = require('../../src/app');
+
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+const mockFile = JSON.stringify({
+  /** usamos o JSON.stringify() para transformar os dados em JSON no mesmo formato em que a função readFile retornaria o arquivo JSON. */
+  brands: [
+    {
+      id: 1,
+      name: 'Lindt & Sprungli',
+    },
+    {
+      id: 2,
+      name: 'Ferrero',
+    },
+    {
+      id: 3,
+      name: 'Ghirardelli',
+    },
+  ],
+  chocolates: [
+    {
+      id: 1,
+      name: 'Mint Intense',
+      brandId: 1,
+    },
+    {
+      id: 2,
+      name: 'White Coconut',
+      brandId: 1,
+    },
+    {
+      id: 3,
+      name: 'Mon Chéri',
+      brandId: 2,
+    },
+    {
+      id: 4,
+      name: 'Mounds',
+      brandId: 3,
+    },
+  ],
+});
+
+describe('Testando a API Cacao Trybe', function () {
+  beforeEach(function () { // antes do cenário de teste nós alteramos o comportamento do método do fs criando um stub
+    sinon.stub(fs.promises, 'readFile') //usamos o sinon criar um stub para a função readFile()
+      .resolves(mockFile);
+    /** que retornará o mockFile contendo todo os dados que estavam no arquivo cacaoTrybeFile.json
+     * conforme especificamos na chamada para resolves. */
+  });
+
+  afterEach(function () { // depois da execução do teste, utilizamos a função restore() que o sinon atribui aos stubs para retornarmos o comportamento padrão daquela função.
+    sinon.restore();
+    // A função restore() vai garantir que o stub de um teste não seja replicado para os testes seguintes.
+  });
+  describe('Usando o método GET em /chocolates', function () {
+    it('Retorna a lista completa de chocolates!', async function () {
+      const output = [
+        { id: 1, name: 'Mint Intense', brandId: 1 },
+        { id: 2, name: 'White Coconut', brandId: 1 },
+        { id: 3, name: 'Mon Chéri', brandId: 2 },
+        { id: 4, name: 'Mounds', brandId: 3 },
+      ];
+
+      const response = await chai
+        .request(app)
+        .get('/chocolates');
+      expect(response.status).to.be.equal(200);
+      expect(response.body.chocolates).to.deep.equal(output);
+    });
+  });
+
+  describe('Usando o método GET em /chocolates/:id para buscar o ID 4', function () {
+    it('Retorna o chocolate Mounds', async function () {
+      const response = await chai
+        .request(app)
+        .get('/chocolates/4');
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body.chocolate).to.deep.equal([
+        {
+          id: 4,
+          name: 'Mounds',
+          brandId: 3,
+        }]);
+    });
+  });
+
+  describe('Usando o método GET em /chocolates/brand/:brandId para buscar brandId 1', function () {
+    it('Retorna os chocolates da marca Lindt & Sprungli', async function () {
+      const response = await chai
+        .request(app)
+        .get('/chocolates/brand/1');
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body.chocolates).to.deep.equal([
+        {
+          id: 1,
+          name: 'Mint Intense',
+          brandId: 1,
+        },
+        {
+          id: 2,
+          name: 'White Coconut',
+          brandId: 1,
+        },
+      ]);
+    });
+  });
+  
+});
